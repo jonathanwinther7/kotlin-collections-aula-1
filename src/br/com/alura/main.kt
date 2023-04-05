5149caa1585f6e8bb40c46dd57ec042d0a754fb0
jonathanwinther7 <jonathanwinther7@outlook.com>
2023-04-05 09:51:32 -0400

    Introdução ao Set

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -1,9 +1,25 @@
 package br.com.alura
 
 fun main() {
-    val assistiramCursoAndroid = listOf<String>("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin = listOf<String>("Alex", "Guia", "Paulo")
-    val assistiramAmbos = assistiramCursoKotlin+ assistiramCursoAndroid
-    println(assistiramAmbos.distinct())
+    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: Set<String> = setOf("Alex", "Maria", "Paulo")
+//    val assistiramAmbos: Set<String> = assistiramCursoAndroid.plus(assistiramCursoKotlin)
+    val assistiramAmbos = mutableSetOf<String>()
+
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("juliete")
+    println(assistiramAmbos)
+
+    assistiramAmbos.remove("juliete")
+    println(assistiramAmbos)
+    println(assistiramCursoKotlin intersect  assistiramCursoAndroid)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
 
 }
